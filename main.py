28641a35778bdd10af109d1c6f8ae3afd96c8234
seav1 <1@seav.eu.org>
2025-09-29 14:16:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def safe_goto(page, url, wait_until="domcontentloaded", timeout=90000):
             return False
     return False
 
-def add_server_time(server_url="https://panel.freegamehost.xyz/server/d09ba3f7"):
+def add_server_time(server_url="https://panel.freegamehost.xyz/server/6b7b7b52"):
     """
     尝试登录 gamepanel2.gtxgaming.co.uk 并点击 "ADD 8 HOUR(S)" 按钮。
     优先使用 REMEMBER_WEB_COOKIE 进行会话登录，如果不存在则回退到邮箱密码登录。
